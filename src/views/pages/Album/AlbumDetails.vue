d22adcb628bbc7ff8251109520c10df349aeef5a
Kuro <kx779966@gmail.com>
2023-04-10 16:33:15 +0200

    Add page Details

--- src/views/pages/Album/AlbumDetails.vue
+++ src/views/pages/Album/AlbumDetails.vue
@@ -1,16 +1,50 @@
 <template>
   <main>
-
+    <h1>{{ album.title }}</h1>
+    <el-table
+        :data="songs"
+        style="width: 99%">
+      <el-table-column
+          prop="title"
+          label="Title">
+      </el-table-column>
+      <el-table-column
+          prop="length"
+          label="Length">
+      </el-table-column>
+      <el-table-column
+          prop="youtube"
+          label="Youtube">
+      </el-table-column>
+    </el-table>
   </main>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'album-details',
   data() {
     return {
-
+      album: {},
+      songs: [],
     };
+  },
+  created() {
+    this.init();
+  },
+  methods: {
+    async init() {
+      const { data } = await axios.get(`/api/albums/${this.$route.params.id}`);
+      this.album = data;
+      await this.loadSongs();
+    },
+    async loadSongs() {
+      this.album.songs.map(async (song) => {
+        this.songs.push(song);
+      });
+    },
   },
 }
 </script>
--- src/views/pages/Artist/ArtistDetails.vue
+++ src/views/pages/Artist/ArtistDetails.vue
@@ -1,26 +1,59 @@
 <template>
   <main>
-
+    <h1>{{ artist.name }}</h1>
+    <el-table
+        :data="albums"
+        style="width: 99%"
+        :class="{'cursor_pointer': true}"
+        @row-click="goToDetails">
+      <el-table-column
+          prop="title"
+          label="Title">
+      </el-table-column>
+      <el-table-column
+          prop="albumtype.name"
+          label="Album Type">
+      </el-table-column>
+      <el-table-column
+          prop="image"
+          label="Image">
+      </el-table-column>
+    </el-table>
   </main>
 </template>
 
 <script>
 import axios from 'axios';
+
 export default {
   name: 'artist-details',
   data() {
     return {
-
+      artist: {},
+      albums: [],
     };
-  },
-  methods: {
-    async init() {
-      // TODO: init details of artist
-    }
   },
   created() {
     this.init();
-  }
+  },
+  methods: {
+    async init() {
+      const { data } = await axios.get(`/api/artists/${this.$route.params.id}`);
+      this.artist = data;
+      await this.loadAlbums();
+    },
+    async loadAlbums() {
+      const albums = this.artist.albums.map(async (albumUrl) => {
+        const albumId = albumUrl.split('/').pop();
+        const { data } = await axios.get(`/api/albums/${albumId}`);
+        return data;
+      });
+      this.albums = await Promise.all(albums);
+    },
+    goToDetails(data) {
+      this.$router.push({name: 'albumsDetails', params: {id: data.id}})
+    },
+  },
 };
 </script>
 
